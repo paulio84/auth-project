47365f96d059102cb186fe190ee244247aee5a4e
Paul Terry <paul.terry84@gmail.com>
2020-07-31 07:49:35 +0100

    ensure firebase is loaded after page refresh

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { auth } from './config/firebase';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App)
-}).$mount('#app');
+// ensure firebase initialises before loading the app when a user refreshes a page
+let app;
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App)
+    }).$mount('#app');
+  }
+});
