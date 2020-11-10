01b2e538e85c8b6f938ec53f231d0cbc525393f5
Paul Terry <paul.terry84@gmail.com>
2020-11-10 19:53:51 +0000

    Implemented react-router and added Home page

--- firebase/auth-react-redux/src/App.jsx
+++ firebase/auth-react-redux/src/App.jsx
@@ -1,14 +1,24 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useBodyClass } from './hooks';
 import Header from './components/Header';
+import Home from './components/pages/Home';
 
 const App = () => {
   useBodyClass('bg-pale');
 
   return (
-    <>
+    <BrowserRouter>
       <Header className="py-4" />
-      <div>Hello World</div>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">{/* <Login /> */}</Route>
+        <Route path="/register">{/* <Register /> */}</Route>
+        <Route path="*">{/* <NotFound /> */}</Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
+
 export default App;
---
+++ firebase/auth-react-redux/src/components/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <section className="container mx-auto px-4">
+    <h1>Home Page</h1>
+  </section>
+);
+
+export default Home;
