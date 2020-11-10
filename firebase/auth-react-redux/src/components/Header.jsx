e9aa8c8273d6f0c48bc07c960ec4bf3351ff567b
Paul Terry <paul.terry84@gmail.com>
2020-11-10 08:38:19 +0000

    Created Header component

---
+++ firebase/auth-react-redux/src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = ({ className }) => (
+  <header className={className}>
+    <h1 className="font-asap text-dark text-2xl">Auth React-Redux</h1>
+  </header>
+);
+
+export default Header;
