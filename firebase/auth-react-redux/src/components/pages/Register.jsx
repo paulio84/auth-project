ed1327d2b5b9d3eeaad6046d5c53d83e739e1017
Paul Terry <paul.terry84@gmail.com>
2020-11-12 19:49:50 +0000

    Built out the register page

--- firebase/auth-react-redux/src/components/pages/Register.jsx
+++ firebase/auth-react-redux/src/components/pages/Register.jsx
@@ -1,7 +1,56 @@
-const Register = () => (
-  <>
-    <h1>Register Page</h1>
-  </>
-);
+import { Link } from 'react-router-dom';
+import { useApplyCSSClass } from '../../hooks';
+
+const Register = () => {
+  useApplyCSSClass('#root', ['h-full', 'md:bg-pale']);
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="bg-white max-w-xs md:max-w-sm pt-6 md:p-6 md:rounded md:shadow">
+          <h1 className="text-center">Sign up for your account</h1>
+          <form className="mt-12 space-y-5 pb-4 border-b border-dark mb-4">
+            <input
+              className="w-full border rounded-sm p-2"
+              type="email"
+              placeholder="Enter email"
+              autoComplete="off"
+              required
+            />
+            <input
+              className="w-full border rounded-sm p-2"
+              type="text"
+              placeholder="Enter full name"
+              autoComplete="off"
+              required
+            />
+            <input
+              className="w-full border rounded-sm p-2"
+              type="text"
+              placeholder="Enter location"
+              autoComplete="off"
+            />
+            <input
+              className="w-full border rounded-sm p-2"
+              type="password"
+              placeholder="Enter password"
+              autoComplete="off"
+              required
+            />
+            <button className="w-full border rounded-sm bg-react-blue p-2">Register</button>
+          </form>
+          <div className="text-center">
+            <span className="text-sm">
+              Already have an account?{' '}
+              <Link to="/login" className="underline">
+                Log In
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Register;
